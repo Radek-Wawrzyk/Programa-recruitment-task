2c6169efe59c3ae333349ddcd18ecd32f1035da8
Radek <radekwawrzyk@gmail.com>
2025-11-09 20:03:33 +0100

    Home - Home Cta section

---
+++ src/components/Home/HomeCta/HomeCta.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import BaseButton from '@/components/Base/BaseButton/BaseButton.vue'
+</script>
+
+<template>
+  <section class="home-cta">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <div class="home-cta__banner">
+            <h2 class="home-cta__title">{{ $t('home.cta.title') }}</h2>
+
+            <BaseButton variant="light" size="lg" class="home-cta__button">
+              {{ $t('home.cta.button') }}
+            </BaseButton>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped lang="scss" src="./HomeCta.scss" />
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import HomeHeader from '@/components/Home/HomeHeader/HomeHeader.vue'
+import HomeCta from '@/components/Home/HomeCta/HomeCta.vue'
 import HomeInfo from '@/components/Home/HomeInfo/HomeInfo.vue'
 </script>
 
 <template>
   <div class="home-view">
     <HomeHeader />
+    <HomeCta />
     <HomeInfo />
   </div>
 </template>
